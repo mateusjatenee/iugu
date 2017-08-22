d35ff64d6af8c2f7be2df76bfee1eec9bde416ae
Mateus Guimarães <mateus.jatene@gmail.com>
2017-08-21 21:50:18 -0300

    docblocks n stuff

--- src/Iugu.php
+++ src/Iugu.php
@@ -6,10 +6,22 @@ use Mateusjatenee\Iugu\Charge;
 
 class Iugu
 {
+    /**
+     * The HTTP client
+     * @var mixed
+     */
     public $client;
 
+    /**
+     * The API token
+     * @var string
+     */
     protected $token;
 
+    /**
+     * @param mixed $client
+     * @param string|null $token
+     */
     public function __construct($client, $token = null)
     {
         $this->client = $client;
@@ -17,33 +29,61 @@ class Iugu
         $this->setAuth($token);
     }
 
+    /**
+     * Returns a Charge instance or performs a direct charge.
+     *
+     * @param array|null $data
+     * @return \Mateusjatenee\Iugu\Charge
+     */
     public function charge($data = null)
     {
         if ($data) {
             return (new Charge($this))->directCharge($data);
         }
+
         return new Charge($this);
     }
 
+    /**
+     * Returns a Transfer instance.
+     *
+     * @return \Mateusjatenee\Iugu\Transfer
+     */
     public function transfers()
     {
         return new Transfer($this);
     }
 
+    /**
+     * Gets the HTTP client
+     *
+     * @return mixed
+     */
     public function getClient()
     {
         return $this->client;
     }
 
+    /**
+     * Sets the API token and basic auth.
+     *
+     * @param string $token
+     * @return $this
+     */
     public function setToken($token)
     {
         $this->token = $token;
 
-        $this->setAuth($token, '');
-
-        return $this;
+        return tap($this)->setAuth($token, '');
     }
 
+    /**
+     * Sets the basic auth headers.
+     *
+     * @param string $user
+     * @param string $password
+     * @return $this
+     */
     public function setAuth($user, $password = '')
     {
         if (is_null($user)) {
@@ -52,13 +92,8 @@ class Iugu
 
         unset($this->client->options['auth']);
 
-        $this->client = $this->client->withBasicAuth($user, $password);
-
-        return $this;
-    }
-
-    public function foo()
-    {
-        return $this->client->foo();
+        return tap($this, function ($iugu) use ($user, $password) {
+            $iugu->client->withBasicAuth($user, $password);
+        });
     }
 }
